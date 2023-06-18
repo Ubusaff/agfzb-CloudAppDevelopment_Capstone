875766993f1aea10d5cf4aec91cdc4608c26a9ed
Ubusaff <peciukelis@email.com>
2023-06-18 11:01:38 -0400

    Week 3

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -68,10 +68,12 @@ def get_dealerships(request):
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
-# def get_dealer_details(request, dealer_id):
-# ...
+def get_dealer_details(request, dealer_id):
+    return HttpResponse(dealer_id)
+
 
 # Create a `add_review` view to submit a review
-# def add_review(request, dealer_id):
-# ...
+def add_review(request, dealer_id):
+    pass
+
 
